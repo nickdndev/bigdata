9c08ec6c2730c5682f49ce10b079e585c160630f
nickdn <nickdndev@gmail.com>
2021-12-09 10:42:01 +0300

    clean code

--- spark_ml_linear_regression/src/main/scala/org/apache/spark/ml/made/LinearRegression.scala
+++ spark_ml_linear_regression/src/main/scala/org/apache/spark/ml/made/LinearRegression.scala
@@ -1,27 +1,25 @@
 package org.apache.spark.ml.made
 
-import breeze.linalg.{sum, DenseVector => BreezeVector}
+import breeze.linalg.{ sum, DenseVector => BreezeVector }
 import org.apache.spark.ml.PredictorParams
-import org.apache.spark.ml.linalg.{DenseVector, Vector, Vectors}
-import org.apache.spark.ml.param.{DoubleParam, IntParam, LongParam, ParamMap}
-import org.apache.spark.ml.regression.{RegressionModel, Regressor}
+import org.apache.spark.ml.linalg.{ DenseVector, Vector, Vectors }
+import org.apache.spark.ml.param.{ DoubleParam, IntParam, ParamMap }
+import org.apache.spark.ml.regression.{ RegressionModel, Regressor }
 import org.apache.spark.ml.stat.Summarizer
 import org.apache.spark.ml.util._
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
-import org.apache.spark.sql.functions.{col, monotonically_increasing_id, rand}
-import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Row}
+import org.apache.spark.sql.functions.rand
+import org.apache.spark.sql.{ DataFrame, Dataset, Encoder, Row }
 
 trait LinearRegressionParams extends PredictorParams {
 
   val numberIterations: IntParam = new IntParam(this, "numberIterations", "Iterations number")
   val learningRate: DoubleParam  = new DoubleParam(this, "learningRate", "Learning Rate")
   val batchSize: IntParam        = new IntParam(this, "batchSize", "Batch Size")
-  val datasetSize: LongParam     = new LongParam(this, "datasetSize", "Dataset Size")
-  val numberBatches: LongParam   = new LongParam(this, "numberBatches", "Number Batches")
 
   setDefault(learningRate, 0.01)
   setDefault(numberIterations, 50)
-  setDefault(batchSize, 50)
+  setDefault(batchSize, 1024)
 
   def setLearningRate(value: Double): this.type = set(learningRate, value)
 
@@ -29,9 +27,6 @@ trait LinearRegressionParams extends PredictorParams {
 
   def setBatchSize(value: Int): this.type = set(batchSize, value)
 
-  def setDatasetSize(value: Long): this.type = set(datasetSize, value)
-
-  def setNumberBatches(value: Long): this.type = set(numberBatches, value)
 }
 
 class LinearRegression(weightsOpt: Option[BreezeVector[Double]], override val uid: String)
@@ -55,10 +50,8 @@ class LinearRegression(weightsOpt: Option[BreezeVector[Double]], override val ui
     val numberFeatures: Int           = MetadataUtils.getNumFeatures(dataset, $(featuresCol))
     var weights: BreezeVector[Double] = weightsOpt.getOrElse(BreezeVector.zeros(numberFeatures + 1))
     val gradientColumnName            = "gradient"
-    setDatasetSize(dataset.count())
-    setNumberBatches($(datasetSize) / $(batchSize))
 
-    val gradientEstimation            = dataset
+    val gradientEstimation = dataset
       .sqlContext
       .udf
       .register(
@@ -70,53 +63,23 @@ class LinearRegression(weightsOpt: Option[BreezeVector[Double]], override val ui
         }
       )
 
-    val calculateGroup = dataset
-      .sqlContext
-      .udf
-      .register(
-        name = uid + "_group_id",
-        func = { (input: Long) =>
-          input % $(numberBatches)
-        }
-      )
-
     for (_ <- 0 to $(numberIterations)) {
-      for (_ <- 0 to ($(datasetSize) / $(batchSize)).toInt) {
-        val transformed_dataset: DataFrame = dataset
-          .orderBy(rand()).limit($(batchSize))
-          //        .withColumn("idx", monotonically_increasing_id())
-          .withColumn(gradientColumnName, gradientEstimation(dataset($(featuresCol)), dataset($(labelCol))))
+      val transformedDataset: DataFrame = dataset
+        .orderBy(rand())
+        .limit($(batchSize))
+        .withColumn(gradientColumnName, gradientEstimation(dataset($(featuresCol)), dataset($(labelCol))))
 
-        //        .withColumn("group_id", calculateGroup(col("idx")))
+      val meanGradient = transformedDataset
+        .select(
+          Summarizer
+            .metrics("mean")
+            .summary(transformedDataset(gradientColumnName))
+        )
+        .first()
 
-        //      val meanGradient = transformed_dataset
-        //        .groupBy(col("group_id"))
-        //        .agg(
-        //          Summarizer
-        //            .metrics("mean")
-        //            .summary(transformed_dataset(gradientColumnName))
-        //        )
-        //        .collect()
-        //
-        //      meanGradient.foreach {
-        //        case Row(_, Row(grad_mean_arr)) =>
-        //          val grad_mean: BreezeVector[Double] = grad_mean_arr.asInstanceOf[DenseVector].asBreeze.toDenseVector
-        //          weights = weights - $(learningRate) * grad_mean
-        //      }
-
-        val meanGradient = transformed_dataset
-          .select(
-            Summarizer
-              .metrics("mean")
-              .summary(transformed_dataset(gradientColumnName))
-          )
-          .first()
-
-        meanGradient match {
-          case Row(Row(grad_mean_arr)) =>
-            val grad_mean: BreezeVector[Double] = grad_mean_arr.asInstanceOf[DenseVector].asBreeze.toDenseVector
-            weights = weights - $(learningRate) * grad_mean
-        }
+      meanGradient match {
+        case Row(Row(meanGradient)) =>
+          weights = weights - $(learningRate) * meanGradient.asInstanceOf[DenseVector].asBreeze.toDenseVector
       }
     }
     val params = Vectors.fromBreeze(weights)
@@ -136,8 +99,7 @@ class LinearRegressionModel protected[made] (override val uid: String, weights: 
   def this(weights: Vector) = this(Identifiable.randomUID("linearRegressionModel"), weights)
 
   override def predict(features: Vector): Double = {
-    val one = BreezeVector(1.0)
-    val x   = BreezeVector.vertcat(one, features.asBreeze.toDenseVector)
+    val x   = BreezeVector.vertcat(BreezeVector(1.0), features.asBreeze.toDenseVector)
     sum(x * weights.asBreeze.toDenseVector)
   }
 
